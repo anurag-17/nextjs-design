6917fcf498a614ad43b8de7103d02f19ed320651
anurag.pandey17 <anurag.pandey17@gmail.com>
2023-11-24 16:21:59 +0530

    sdefs

--- component/button/Button.jsx
+++ component/button/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return(
-  <button className=" inline-flex justify-center items-center gap-10 px-[45px] py-[17px] border border-[#54A3F2]   rounded-[50px] bg-[#54A3F2]  text-white font-semibold leading-[26px] font-[Montserrat] uppercase hover:text-[#54A3F2] transition hover:bg-white">
+  <button className=" inline-flex justify-center items-center gap-10 px-[45px] py-[10px] lg:py-[17px] border border-[#54A3F2] rounded-[50px] bg-[#54A3F2]  text-white font-semibold leading-[26px] font-[Montserrat] uppercase hover:text-[#54A3F2] transition hover:bg-white w-full md:w-auto">
   shop now
 </button>
 )
