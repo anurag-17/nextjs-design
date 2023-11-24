ab845a7eca2e5b480937819caf91d97b39ac81d3
anurag.pandey17 <anurag.pandey17@gmail.com>
2023-11-24 15:41:22 +0530

    correct movile

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="/svg/logo.svg" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
